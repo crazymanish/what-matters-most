306f4f0ca99e30146fdb09858b63694abd0cfa72
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-03-27 20:10:48 +0200

    Added empty CombineDemo project

---
+++ CombineDemo/CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by Manish Rathi on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
