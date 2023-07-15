e5d8a5a60d954c62717376e9e7bb056ea0f1b922
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-07-15 20:43:55 +0200

    OR-5264 Operators:: Time-manipulation Operators: CollectingValues by time

--- CombineDemo/CombineDemoTests/Operators/TimeManipulationOperators/CollectByTimeTests.swift
+++ CombineDemo/CombineDemoTests/Operators/TimeManipulationOperators/CollectByTimeTests.swift
@@ -1,5 +1,5 @@
 //
-//  ShiftingTimeTests.swift
+//  CollectByTimeTests.swift
 //  CombineDemoTests
 //
 //  Created by Manish Rathi on 15/07/2023.
@@ -9,12 +9,13 @@ import Foundation
 import Combine
 import XCTest
 
-/// - `delay(for:tolerance:scheduler:options:)` Delays delivery of all output to the downstream receiver by a specified amount of time on a particular scheduler.
-/// - https://developer.apple.com/documentation/combine/publishers/reduce/delay(for:tolerance:scheduler:options:)
-///
-/// - Use delay(for:tolerance:scheduler:options:) when you need to delay the delivery of elements to a downstream by a specified amount of time.
-/// - In this example, a Timer publishes an event every second. The delay(for:tolerance:scheduler:options:) operator holds the delivery of the initial element for 3 seconds (±0.5 seconds), after which each element is delivered to the downstream on the main run loop after the specified delay:
-final class ShiftingTimeTests: XCTestCase {
+/// - `collect(_:options:)` Collects elements by a given time-grouping strategy, and emits a single array of the collection.
+/// - https://developer.apple.com/documentation/combine/publishers/reduce/collect(_:options:)
+/// - Use collect(_:options:) to emit arrays of elements on a schedule specified by a Scheduler and Stride that you provide. At the end of each scheduled interval, the publisher sends an array that contains the items it collected.
+/// - If the upstream publisher finishes before filling the buffer, the publisher sends an array that contains items it received. This may be fewer than the number of elements specified in the requested Stride.
+
+/// - If the upstream publisher fails with an error, this publisher forwards the error to the downstream receiver instead of sending its output.
+final class CollectByTimeTests: XCTestCase {
     var cancellables: Set<AnyCancellable>!
     var isFinishedCalled: Bool!
 
@@ -32,37 +33,35 @@ final class ShiftingTimeTests: XCTestCase {
         super.tearDown()
     }
 
-    func testPublisherWithDelayOperator() {
-        let expectation = XCTestExpectation(description: "Testing delay operator")
+    func testPublisherWithCollectByTimeOperator() {
+        let expectation = XCTestExpectation(description: "Testing collect with time strategy operator")
 
         var sentValues: [String] = []
-
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .none
         dateFormatter.timeStyle = .long
 
-        Timer.publish(every: 1.0, on: .main, in: .default)
+        Timer.publish(every: 1.0, on: .main, in: .common)
             .autoconnect()
             .handleEvents(receiveOutput: { date in
                 let dateTimeValue = (dateFormatter.string(from: date))
                 sentValues.append(dateTimeValue)
             })
-            .delay(for: .seconds(3), scheduler: RunLoop.main, options: .none)
-            .collect(2)
+            .collect(.byTime(DispatchQueue.main, .seconds(4))) // Collecting value
             .sink { [weak self] completion in
                 switch completion {
                 case .finished:
                     self?.isFinishedCalled = true
                 }
             } receiveValue: { values in
-                let now = Date()
+                print(sentValues) // Sent values
+                print("--------------")
+                print(values.map { dateFormatter.string(from: $0) }) // received values
 
                 for (index, value) in values.enumerated() {
                     let receivedDateTimeValue = dateFormatter.string(from: value)
-                    let receivedValueAfterDelay = String(format: "%.0f", now.timeIntervalSince(value))
 
                     XCTAssertEqual(receivedDateTimeValue, sentValues[index]) // Received correct values in exactly same order after delay
-                    XCTAssertEqual(receivedValueAfterDelay, "\(4-index)") // Received after correct delay (diff is always 1 second because above Timer is publishing new value in every second)
                 }
 
                 expectation.fulfill()
